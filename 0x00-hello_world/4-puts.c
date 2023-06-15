018fb4b6a15fef249b35b622b60e94affed78d3f
Destiny Arupi <destinyarupi@gmail.com>
2023-06-15 06:56:50 -0700

    task-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   put("\"Programming is like building a multilingual puzzle");
+   return(0);
+}
