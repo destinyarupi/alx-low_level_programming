7fec7d339031ba373193193cddb2db115049ca6b
Destiny Arupi <destinyarupi@gmail.com>
2023-06-20 02:37:29 -0700

    tasks completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('D');
+	_putchar('e');
+	_putchar('s');
+	_putchar('t');
+	_putchar('i');
+	_putchar('n');
+	_putchar('y');
+	_putchar('\n');
+	return (0);
+}
